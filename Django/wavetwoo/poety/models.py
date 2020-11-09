ef8ad73fd1a01043510ae4e0ef1c15a81545660d
ArthurNeyer <arthur.neyer@protonmail.com>
2020-11-09 16:27:41 +0100

    Update models poety

--- Django/wavetwoo/poety/models.py
+++ Django/wavetwoo/poety/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 
 # Create your models here.
@@ -7,7 +8,7 @@ class SnapPoety(models.Model):
     title = models.CharField(max_length=63)
     image = models.ImageField(upload_to='images/snappoety')
     text = models.TextField(max_length=2047)
-    date = models.DateField(auto_now_add=True)
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return '{0} : {1} ({2})'.format(self.title, self.text, self.date)
